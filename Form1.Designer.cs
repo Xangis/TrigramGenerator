599d178e3464c72407c8d48c9b945877e9e82fc0
Jason Champion <jchampion@zetacentauri.com>
2020-12-10 20:50:11 -0800

    Start of Trigram Generator rewrite with Windows Forms and .net.

---
+++ Form1.Designer.cs
@@ -0,0 +1,175 @@
+﻿namespace TrigramGenerator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnGenerate = new System.Windows.Forms.Button();
+            this.btnAbout = new System.Windows.Forms.Button();
+            this.txtInput = new System.Windows.Forms.TextBox();
+            this.txtOutput = new System.Windows.Forms.TextBox();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.btnLoad = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(15, 71);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(58, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Input Text:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 260);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(66, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Output Text:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(71, 168);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(63, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "# of Words:";
+            // 
+            // btnGenerate
+            // 
+            this.btnGenerate.Location = new System.Drawing.Point(268, 163);
+            this.btnGenerate.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnGenerate.Name = "btnGenerate";
+            this.btnGenerate.Size = new System.Drawing.Size(64, 25);
+            this.btnGenerate.TabIndex = 3;
+            this.btnGenerate.Text = "Generate";
+            this.btnGenerate.UseVisualStyleBackColor = true;
+            this.btnGenerate.Click += new System.EventHandler(this.BtnGenerate_Click);
+            // 
+            // btnAbout
+            // 
+            this.btnAbout.Location = new System.Drawing.Point(337, 163);
+            this.btnAbout.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnAbout.Name = "btnAbout";
+            this.btnAbout.Size = new System.Drawing.Size(64, 25);
+            this.btnAbout.TabIndex = 4;
+            this.btnAbout.Text = "About";
+            this.btnAbout.UseVisualStyleBackColor = true;
+            this.btnAbout.Click += new System.EventHandler(this.BtnAbout_Click);
+            // 
+            // txtInput
+            // 
+            this.txtInput.Location = new System.Drawing.Point(75, 11);
+            this.txtInput.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.txtInput.Multiline = true;
+            this.txtInput.Name = "txtInput";
+            this.txtInput.Size = new System.Drawing.Size(326, 147);
+            this.txtInput.TabIndex = 5;
+            // 
+            // txtOutput
+            // 
+            this.txtOutput.Location = new System.Drawing.Point(75, 192);
+            this.txtOutput.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.txtOutput.Multiline = true;
+            this.txtOutput.Name = "txtOutput";
+            this.txtOutput.Size = new System.Drawing.Size(326, 147);
+            this.txtOutput.TabIndex = 6;
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(136, 167);
+            this.numericUpDown1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(59, 20);
+            this.numericUpDown1.TabIndex = 7;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            // 
+            // btnLoad
+            // 
+            this.btnLoad.Location = new System.Drawing.Point(200, 163);
+            this.btnLoad.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnLoad.Name = "btnLoad";
+            this.btnLoad.Size = new System.Drawing.Size(64, 25);
+            this.btnLoad.TabIndex = 8;
+            this.btnLoad.Text = "Load";
+            this.btnLoad.UseVisualStyleBackColor = true;
+            this.btnLoad.Click += new System.EventHandler(this.BtnLoad_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(412, 347);
+            this.Controls.Add(this.btnLoad);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.txtOutput);
+            this.Controls.Add(this.txtInput);
+            this.Controls.Add(this.btnAbout);
+            this.Controls.Add(this.btnGenerate);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Name = "Form1";
+            this.Text = "Lamda Trigram Generator";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnGenerate;
+        private System.Windows.Forms.Button btnAbout;
+        private System.Windows.Forms.TextBox txtInput;
+        private System.Windows.Forms.TextBox txtOutput;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Button btnLoad;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,213 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TrigramGenerator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnGenerate_Click(object sender, EventArgs e)
+        {
+  //          memset(_trigramWeights, 0, sizeof(_trigramWeights));
+  //          memset(_totalWeight, 0, sizeof(_totalWeight));
+  //          wxString inputText = _txtInput->GetValue();
+  //          int numWords = _spnNumWords->GetValue();
+
+  //          if (!inputText.Len())
+  //          {
+  //              wxMessageBox("Nothing to base output on.");
+  //              return;
+  //          }
+
+  //          const char* input = inputText.mb_str();
+  //          int length = (int)strlen(input);
+  //          int count;
+  //          int num = 0;
+
+  //          // Set first letter to space.
+  //          _previousPreviousLetter = 0;
+  //          _previousLetter = GetLetterNumber(input[0]);
+
+  //          // We start at one because we set the initial letter above.
+  //          for (count = 1; count < length; count++)
+  //          {
+  //              num = GetLetterNumber(input[count]);
+  //              if (num != -1)
+  //              {
+  //                  _trigramWeights[_previousPreviousLetter][_previousLetter][num]++;
+  //                  _totalWeight[_previousPreviousLetter][_previousLetter]++;
+  //                  _previousPreviousLetter = _previousLetter;
+  //                  _previousLetter = num;
+  //              }
+  //          }
+
+  //          // When generating, start with the previous letter as a space so we get realistic word beginnings.
+  //          _previousLetter = 0;
+
+  //          // TODO: Set _previousLetter to a digram generation from chart.
+  //          _previousLetter = GetLetterNumber(input[0]);
+  //          _previousPreviousLetter = 0;
+
+  //          wxString outputText = input[0];
+  //          outputText += GenerateRandomLetter(false);
+
+  //          int words = 0;
+  //          int letters = 0;
+  //          while (words < numWords)
+  //          {
+  //              char newletter = GenerateRandomLetter(true);
+  //              if (newletter == 32)
+  //              {
+  //                  words++;
+  //                  outputText += "\n";
+  //              }
+  //              else
+  //              {
+  //                  outputText += newletter;
+  //              }
+  //              letters++;
+  //              _previousPreviousLetter = _previousLetter;
+  //              _previousLetter = GetLetterNumber(newletter);
+  //              if (letters > 12000)
+  //              {
+  //                  _txtOutput->SetValue("Generated output invalid.  Are you sure you have enough input text?");
+
+  //          event.Skip();
+		//	return;
+		//}
+  //  }
+
+  //  char outputExamine[4096];
+  //  memset(outputExamine, 0, 4096 );
+  //  memcpy(outputExamine, outputText.mb_str(), outputText.Len() );
+  //  _txtOutput->SetValue(outputText);
+
+  //  event.Skip();
+}
+
+        private void BtnAbout_Click(object sender, EventArgs e)
+        { 
+        //{
+        //    // Show about box.
+        //    wxAboutDialogInfo info;
+        //    info.SetName(_("Trigram Generator"));
+        //    info.SetVersion(_("1.2"));
+        //    info.SetCopyright(_("(c) 2006-2020 Lambda Centauri"));
+        //    info.AddDeveloper(_("Jason Champion"));
+        //    info.SetIcon(_icon);
+        //    info.SetLicense(_("This application is distributed under the terms of the MIT License."));
+        //    info.SetWebSite(_("https://github.com/Xangis/Trigram"));
+        //    info.SetDescription(_("Trigram Generator uses the wxWidgets libraries."));
+
+        //    wxAboutBox(info);
+
+        //     event.Skip();
+        }
+
+        private void BtnLoad_Click(object sender, EventArgs e)
+        {
+    //        wxString path = _(".\\");
+    //        wxFileDialog fdialog(this, _("Load Text File"), path, _(""), _("Text Files (*.txt) |*.txt||"), wxFD_OPEN );
+
+    //        wxString filename;
+
+    //        if (fdialog.ShowModal() != wxID_OK)
+    //        {
+    //            return;
+    //        }
+
+    //        _txtInput->Clear();
+
+    //        wxString fname = fdialog.GetPath();
+
+    //        wxTextFile tfile;
+    //        tfile.Open(fname);
+    //        while (!tfile.Eof())
+    //        {
+    //            _txtInput->AppendText(tfile.GetNextLine() + "\n");
+    //        }
+    //        tfile.Close();
+
+    //event.Skip();
+        }
+    }
+}
+
+
+// Case insensitive, gets array position of a particular letter.
+//int MainDlg::GetLetterNumber(char letter)
+//{
+//    // Convert newlines to spaces.
+//    if (letter == 10)
+//    {
+//        return 0;
+//    }
+//    if (letter < 32 || letter > 126)
+//    {
+//        return -1;
+//    }
+//    return (letter - 32);
+//}
+
+//char MainDlg::GenerateRandomLetter(bool trigram)
+//{
+//    int numOptions = _totalWeight[_previousPreviousLetter][_previousLetter];
+
+//    // This should NEVER happen.
+//    if (numOptions == 0)
+//    {
+//        return '@';
+//    }
+
+//    int selection = rand() % numOptions;
+//    int count;
+//    int total = 0;
+
+//    if (trigram)
+//    {
+//        for (count = 0; count < 95; count++)
+//        {
+//            total += _trigramWeights[_previousPreviousLetter][_previousLetter][count];
+//            if (selection < total)
+//            {
+//                return GetLetter(count);
+//            }
+//        }
+//    }
+//    else
+//    {
+//        for (count = 0; count < 95; count++)
+//        {
+//            //total += _trigramWeights[_previousLetter][count][0];
+//            total += _trigramWeights[0][_previousLetter][count];
+//            if (selection < total)
+//            {
+//                return GetLetter(count);
+//            }
+//        }
+//    }
+//    // PANIC!  No match, our logic is broken.
+//    return '*';
+//}
+//char MainDlg::GetLetter(int number)
+//{
+//    if (number > 0 || number < 95)
+//    {
+//        return (number + 32);
+//    }
+//    else
+//    {
+//        return 0;
+//    }
+//}
